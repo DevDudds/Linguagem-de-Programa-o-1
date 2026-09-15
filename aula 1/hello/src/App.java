086345edbffeedc8d1f1d8a20559aff3cc258b3f
Dudds <duddscosta@gmail.com>
2026-09-15 19:56:25 -0300

    commit da primeira aula

---
+++ aula 1/hello/src/App.java
@@ -0,0 +1,41 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.err.println("Hello, World!");
+
+        // Números
+        int inteiro = 1;
+        long inteiro2x = 2;
+
+        float decimais = (float) 1.00000001;
+        double float2x = 1.00000000001;
+
+        // logico
+
+        boolean bool = false;
+        boolean bool2 = true;
+
+        // textual
+
+        String texto = "Eduardo";
+        char character = 'E';
+
+        // Operadores
+        // + - / *
+        // Booleano
+        // < > <= >= != ==
+        // && || !
+        boolean teste = true;
+        teste = !teste;
+        System.out.println(!teste);
+
+        int numeros[] = {1,2,3,4,5,6,7,8};
+
+        for (int n : numeros){
+            System.out.println(n);
+        }
+
+        // metodos
+
+        
+    }
+}
